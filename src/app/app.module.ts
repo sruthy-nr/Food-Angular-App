caadf039aacdb5ce3426b9e055e5a1810b283899
sruthy-nr <sruthyn.@nestgroup.net>
2022-11-25 01:20:04 +0530

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,34 @@ import { ViewComponent } from './view/view.component';
 import { OrderComponent } from './order/order.component';
 import { PaymentComponent } from './payment/payment.component';
 import { FeedbackComponent } from './feedback/feedback.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const myRoute:Routes=[
+  {
+    path:"",
+    component:RegistrationComponent
+  },
+  {
+    path:"login",
+    component:LoginComponent
+  },
+  {
+    path:"view",
+    component:ViewComponent
+  },
+  {
+    path:"order",
+    component:OrderComponent
+  },
+  {
+    path:"payment",
+    component:PaymentComponent
+  },
+  {
+    path:"feedback",
+    component:FeedbackComponent
+  }
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,7 +49,8 @@ import { FeedbackComponent } from './feedback/feedback.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
